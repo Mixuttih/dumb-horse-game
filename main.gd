462d76738c0330c37c8ff6aad0f7a4f4c5cb2b1e
Mixuttih <alanhommis@gmail.com>
2023-12-03 15:08:42 +0200

    middle mesh tweaking
    changed middle mesh to cylinder that faces players

--- main.gd
+++ main.gd
@@ -19,20 +19,20 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	mid_point_x = (player_1.global_position.x + player_2.global_position.x) / 2
-	mid_point_y = (player_1.global_position.y + player_2.global_position.y) / 2
+	mid_point_y = ((player_1.global_position.y + player_2.global_position.y) / 2) + 1
 	mid_point_z = (player_1.global_position.z + player_2.global_position.z) / 2
 	mid_point_calc = Vector3(mid_point_x, mid_point_y, mid_point_z)
 	middle_mesh.global_position = mid_point_calc
 	
 	area_3d.position = middle_mesh.global_position
+	middle_mesh.look_at(player_1.position)
+	middle_mesh.rotation_degrees.x = 90
 	
 
 func _on_area_3d_body_exited(body):
 	print(body)
 	Globals.speed_player_1 = 2.5
 	Globals.speed_player_2 = 2.5
-
-
 
 func _on_area_3d_body_entered(body):
 	print(body)
