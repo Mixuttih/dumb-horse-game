0555b8d1629d793da8ad5ef2a999f2e2ae6e4670
Mixuttih <alanhommis@gmail.com>
2023-12-15 13:47:36 +0200

    middle mesh improvements
    added middle mesh stretching, added extra velocity for players mid-air, changed the game over mechanics

--- main.gd
+++ main.gd
@@ -9,8 +9,6 @@ extends Node3D
 
 @onready var player_1 = $player1
 @onready var player_2 = $player2
-@onready var break_timer = $BreakTimer
-@onready var break_label = $BreakLabel
 @onready var game_over_label = $GameOver
 
 
@@ -31,12 +29,12 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if mesh.mesh.radius <= 0.08:
+		middle_break()
 	
 	if middle_mesh.visible == false:
 		game_over()
 	
-	#Update the timer display every frame
-	break_label.set_text(str(break_timer.get_time_left()).pad_decimals(1))
 	
 	#Calculate the mid-point of two players
 	mid_point_x = (player_1.global_position.x + player_2.global_position.x) / 2
@@ -48,58 +46,47 @@ func _process(delta):
 	middle_mesh.global_position.z = player_2.global_position.z
 	middle_mesh.global_position.y = player_2.global_position.y + 1.5
 	
-	
-	
 	#Rotate the middle mesh to player 1
 	middle_mesh.look_at(Vector3(player_1.position.x, player_1.position.y + 1.5, player_1.position.z))
-	
 	
 	
 	#Pull player 1 towards middle mesh
 	player_1.velocity.x = -(player_1.global_position.x - mid_point_calc.x)
 	player_1.velocity.z = -(player_1.global_position.z - mid_point_calc.z)
-	#Pull player 2 towards middle mesh
+	
 	player_2.velocity.x = -(player_2.global_position.x - mid_point_calc.x)
 	player_2.velocity.z = -(player_2.global_position.z - mid_point_calc.z)
+	
+	if player_1.is_on_floor() == false:
+		player_1.velocity.x = player_1.velocity.x * (player_1.global_position.distance_to(player_2.global_position) * 2)
+		player_1.velocity.z = player_1.velocity.z * (player_1.global_position.distance_to(player_2.global_position) * 2)
+	if player_2.is_on_floor() == false:
+		player_2.velocity.x = player_2.velocity.x * (player_2.global_position.distance_to(player_1.global_position) * 2)
+		player_2.velocity.z = player_2.velocity.z * (player_2.global_position.distance_to(player_1.global_position) * 2)
 	
 	#Calculate the distance of player 1 and player 2
 	player_distance = abs(player_1.global_position - player_2.global_position)
 	
 	#Stretch the middle mesh based on distance
-	mesh.mesh.height = 0.5 + player_1.global_position.distance_to(player_2.global_position)
+	mesh.mesh.height = 0.3 + player_1.global_position.distance_to(player_2.global_position)
+	mesh.mesh.radius =  0.5 - (player_1.global_position.distance_to(player_2.global_position) * 0.05)
 	
 	#Check player distance from each other
 	#Adjust player speed
-	#Start and show timer if players far enough
+
 	if player_distance.x + player_distance.z < 5:
 		Globals.speed_player_1 = 5.0
 		Globals.speed_player_2 = 5.0
-		if timer_running == true:
-			timer_stop()
+
 	elif player_distance.x + player_distance.z > 5:
 		Globals.speed_player_1 = 2
 		Globals.speed_player_2 = 2
-		if timer_running == false:
-			timer_start()
 
 
-
-func timer_start():
-	timer_running = true
-	break_timer.start()
-	break_label.visible = true
-
-func timer_stop():
-	timer_running = false
-	break_timer.stop()
-	break_label.visible = false
-	
-
-func _on_break_timer_timeout():
+func middle_break():
 	middle_mesh.visible = false
 
 func game_over():
-	break_timer.stop()
 	player_1.process_mode = Node.PROCESS_MODE_DISABLED
 	player_2.process_mode = Node.PROCESS_MODE_DISABLED
 	game_over_label.visible = true
--- player_2.gd
+++ player_2.gd
@@ -4,7 +4,7 @@ extends CharacterBody3D
 @onready var horsearse_2 = $horsearse2
 
 var SPEED = Globals.speed_player_2
-var JUMP_VELOCITY = 4.5
+var JUMP_VELOCITY = 5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
